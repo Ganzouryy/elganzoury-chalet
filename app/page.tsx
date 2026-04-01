b3f8e1fe3bf533cbd84f0c65bb6ca3cf13b7a493
Ganzouryy <ganzouryyy@gmail.com>
2026-04-01 15:40:38 +0200

    static export for cloudflare pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,5 @@
-import { supabase } from './lib/supabase'
 import ChaletWebsite from './components/ChaletWebsite'
 
-export default async function Home() {
-  const { data: pricing } = await supabase.from('pricing').select('*')
-  const { data: media } = await supabase.from('media').select('*').order('sort_order')
-  const { data: content } = await supabase.from('site_content').select('*')
-
-  return (
-    <ChaletWebsite
-      pricing={pricing || []}
-      media={media || []}
-      content={content || []}
-    />
-  )
+export default function Home() {
+  return <ChaletWebsite pricing={[]} media={[]} content={[]} />
 }
